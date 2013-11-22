e60317375f596d883191bf2f8edae47af1f81425
Catherine Lu <catherinelu@rescomp-13-270049.stanford.edu>
2013-11-22 01:53:37 -0800

    More naming changes

--- create_symbol_data.py
+++ create_symbol_data.py
@@ -8,8 +8,9 @@ relative_path2012 = 'ICFHR_package/CROHME2012_data/trainData/trainData/'
 relative_testpath2011 = 'ICFHR_package/CROHME2011_data/CROHME_testGT/CROHME_testGT/'
 relative_testpath2012 = 'ICFHR_package/CROHME2012_data/testDataGT/'
 
-def getTrainData(pathArray):
-  trainData = []
+def getData(pathArray):
+  dataX = []
+  dataY = []
   from time import time
   start = time()
   for path in pathArray:
@@ -19,12 +20,24 @@ def getTrainData(pathArray):
       traceList, symbolsList = gettrace.parseINKMLFile(path + file)
       for label, indices in symbolsList:
           pixels = itp.inkml_to_pixels([traceList[elem] for elem in indices])
-          trainData.append((pixels, label))
+          dataX.append(pixels)
+          dataY.append(label)
           # itp.display(pixels)
   print time()-start
-  print len(trainData)
-  return trainData
+  print len(dataX)
+  return [dataX, dataY]
+
+def createIntegerLabelMap(labels):
+  labels_set = set(labels)
+  labels_dict = {label : i for i, label in enumerate(labels_set)}
+  return labels_dict
+
+def translateLabels(label_map, labels):
+  translated_labels = []
+  for label in labels:
+    translated_labels.append(label_map[label])
+  return translated_labels
 
 if __name__ == "__main__":
-  # getTrainData([relative_path2011, relative_path2012])
-  getTrainData([relative_testpath2011, relative_testpath2012])
+  # getData([relative_path2011, relative_path2012])
+  getData([relative_testpath2011, relative_testpath2012])
